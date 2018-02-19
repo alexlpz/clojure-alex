89943c0a3f72ea5ca1fc7130d83114af3b53826d
alexlpz <lpz.vlz.ale@gmail.com>
2018-02-19 16:06:40 -0600

    second commit

---
+++ src/app/core.clj
@@ -0,0 +1,20 @@
+(ns app.core
+(:gen-class))
+
+(defn foo [x]
+  (println x "Hello, World!"))
+
+  (defn fibonacci [x]
+    (if (<= x 2) 1
+        (+ (fibonacci (- x 2)) (fibonacci (- x 1))) ))
+
+
+      (defn factorial [x]
+        (if (<= x 1) 1
+        (* (factorial (- x 1)) x)))
+
+  (defn -main [& args]
+    (let [arg-1 (or (first args) "0")
+    n (Integer/parseInt arg-1)]
+    (println "fib("n") =" (fibonacci n))
+    (println "!("n") =" (factorial n))))
